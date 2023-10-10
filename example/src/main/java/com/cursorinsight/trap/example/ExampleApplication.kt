833a3e74b93dabea7c0a2d02bc85395576e66fc5
Denes Sapi <denes@cursorinsight.com>
2023-10-10 23:40:08 +0200

    feat: Update the README and the example application

---
+++ example/src/main/java/com/cursorinsight/trap/example/ExampleApplication.kt
@@ -0,0 +1,38 @@
+package com.cursorinsight.trap.example
+
+import android.app.Application
+import com.cursorinsight.trap.TrapConfig
+import com.cursorinsight.trap.TrapManager
+import org.json.JSONObject
+
+class ExampleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val config = TrapConfig()
+
+// Can be a websocket or http server
+        config.reporter.url = "https://example.com/api/post/{streamId}/{sessionId}"
+
+// Set reporting interval to 3 seconds
+        config.reporter.interval = 3000
+
+// Set session id
+        config.initSessionId(this)
+
+// Instantiate TrapManager
+        val trapManager = TrapManager.getInstance(this, config)
+
+// Add a custom metadata
+        trapManager.addCustomMetadata("uid", "some-uid")
+
+// Add a custom event
+        trapManager.addCustomEvent(with(JSONObject()) {
+            put("some-key", "some-data")
+            put("numeric-data-key", 2)
+            put("boolean-data-key", false)
+            this
+        })
+    }
+}
